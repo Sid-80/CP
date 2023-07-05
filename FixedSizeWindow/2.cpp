90d45cd9780219019326f8de3c25735f8ededf4b
Sid-80 <siddharth.brahmecha@trainingmug.com>
2023-07-06 00:00:50 +0530

    3 done

---
+++ FixedSizeWindow/2.cpp
@@ -0,0 +1,42 @@
+// Given an array A[] of size N and a positive integer K, 
+// find the first negative integer for each and every window(contiguous subarray) of size K.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+     int max = INT_MIN;
+    int N;
+    cout<<"Enter size : ";cin>>N;
+    int arr[N];
+    for(int i = 0;i < N; i++){
+        cin>>arr[i];
+    }
+    int K;
+    cout<<"Enter the size of subarray : ";cin>>K;
+
+    int i = 0, j = 0;
+    list<int> l1;
+    while(j < N){
+        if((j-i+1) < K){
+
+            if(arr[j] < 0)
+                l1.push_back(arr[j]);
+            
+            j++;
+        
+        }else if((j-i+1) == K){
+            if(!l1.empty())
+                cout<<l1.front()<<" ";
+            else
+                cout<<"0"<<" ";
+            
+            if(l1.front() == arr[i])
+                l1.pop_front();
+            i++;
+        }
+    }
+
+    return 0;
+}
---
+++ FixedSizeWindow/3.cpp
@@ -0,0 +1,68 @@
+// COUNT OCCURENCES OF ANAGRAM
+// Given a word pat and a text txt. Return the count of the occurrences of anagrams of the word in the text.
+// Input:
+// txt = forxxorfxdofr
+// pat = for
+// Output: 3
+// Explanation: for, orf and ofr appears
+// in the txt, hence answer is 3.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string txt, pat;
+    cout << "Enter the text : ";
+    cin >> txt;
+    cout << "Enter the pattern : ";
+    cin >> pat;
+
+    map<char, int> m1;
+
+    for (int i = 0; i < pat.length(); i++)
+    {
+        if (m1.find(pat[i]) == m1.end())
+        {
+            m1.insert(pair<char, int>(pat[i], 1));
+        }
+        else
+        {
+            m1[pat[i]] = m1[pat[i]]++;
+        }
+    }
+
+    int count = m1.size();
+
+    int i = 0, j = 0;
+    int ans = 0;
+
+    while (j != txt.length() - 1)
+    {
+        if ((j - i + 1) < pat.length())
+        {
+            if (m1.find(txt[j]) != m1.end())
+            {
+                m1[txt[j]]--;
+                if (m1[txt[j]] == 0)
+                {
+                    count -= 1;
+                }
+                if (count == 0)
+                    ans += 1;
+            }
+            j++;
+        }
+        else if ((j - i + 1) == pat.length())
+            {
+                if(m1.find(txt[i]) != m1.end()){
+                    m1[txt[i]]++;
+                    count += 1;
+                }
+                i++;
+            }
+        
+    }
+    cout << ans << endl;
+    return 0;
+}
