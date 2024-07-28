e5f2f8a69e0f03bd11f886ec8c7995a14b5a08eb
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2024-07-28 16:24:45 +0530

    Time: 28 ms (5.36%) | Memory: 9 MB (31.10%) - LeetSync

---
+++ 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canTransform(string s, string e) {
+        int i = 0, j = 0, n = s.size(), m = e.size();
+        while(i < n || j < m) {
+            while(s[i] == 'X') i++;
+            while(e[j] == 'X') j++;
+            cout<<i<<" "<<j<<endl;
+            if(s[i] != e[j]) return false;
+            if(s[i] == 'R' && i > j) return false;
+            if(s[i] == 'L' && i < j) return false; 
+            i++, j++;
+        }
+        return true;
+    }
+};
