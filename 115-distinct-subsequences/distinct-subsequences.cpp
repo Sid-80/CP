f6d694fd08302af64b90bf2727da617d65282779
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2024-07-30 18:45:43 +0530

    Time: 19 ms (57.70%) | Memory: 31.7 MB (23.62%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -18,24 +18,30 @@ public:
         int n  = s.size(); 
         int m  = t.size();
 
-        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        vector<vector<double>> dp(n+1, vector<double> (m+1,0));
 
-        return getAns(n,m,s,t,dp);
+        // return getAns(n,m,s,t,dp);
 
-        // for(int i = n;i >= 0;i--){
-        //     for(int j = m;j >= 0;j--){
-        //         if(j == 0) return 1;
-        //         if(i == 0) return 0;
+        for(int i = 0;i <= n;i++){
+            for(int j = 0;j <= m;j++){
+                if(j == 0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if(i == 0){
+                    dp[i][j] = 0;
+                    continue;
+                }
 
-        //         if(s[i-1] == t[j-1]){
-        //             dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-        //         }else{
-        //             dp[i][j] = dp[i-1][j];
-        //         }
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
 
-        //     }
-        // }
+            }
+        }
 
-        // return dp[n][m]; 
+        return (int)dp[n][m]; 
     }
 };
