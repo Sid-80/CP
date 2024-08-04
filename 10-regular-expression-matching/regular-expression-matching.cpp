c650c8ee0ededff9b34a618c848a577d86ac951c
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2024-08-04 18:50:14 +0530

    Time: 6 ms (38.53%) | Memory: 9 MB (43.91%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool helper(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+
+        int n = s.size(), m = p.size();
+        if (j == m) {
+            return (i == n);
+        }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        bool nextStar = false;
+        if (j + 1 < m && p[j + 1] == '*')
+            nextStar = true;
+        if (nextStar) {
+            bool a = false, b = false;
+            // take
+            if (i < n && (s[i] == p[j] || p[j] == '.')) {
+
+                a = helper(i + 1, j, s, p, dp);
+            }
+            // not take
+            b = helper(i, j + 2, s, p, dp);
+            return dp[i][j] = (a || b);
+        } else {
+            if (i < n && (s[i] == p[j] || p[j] == '.'))
+                return dp[i][j] = helper(i + 1, j + 1, s, p, dp);
+        }
+        return dp[i][j] = false;
+    }
+
+    bool isMatch(string s, string p) {
+        int n1 = s.size(), n2 = p.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+
+        return helper(0, 0,  s, p, dp);
+    }
+};
