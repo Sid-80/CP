81af1474747d220e6ab3392446bea8fff11c8a5e
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2025-02-27 10:43:22 +0530

    Time: 95 ms (77.55%) | Memory: 14.1 MB (7.58%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,42 +1,53 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int> nums, vector<vector<int>>& dp) {
-        if (i > j)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int cost = INT_MIN;
-        for (int k = i; k <= j; k++) {
-            int mini = nums[i - 1] * nums[k] * nums[j + 1] +
-                       solve(i, k - 1, nums, dp) + solve(k + 1, j, nums, dp);
-            cost = max(cost, mini);
-        }
-
-        return dp[i][j] = cost;
-    }
 
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        nums.push_back(1);
-        nums.insert(nums.begin(), 1);
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        vector<int> g;
 
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= n; j++) {
-                if (i > j) continue;
-
-                int cost = INT_MIN;
-                for (int k = i; k <= j; k++) {
-                    int mini = nums[i - 1] * nums[k] * nums[j + 1] +
-                               dp[i][k - 1] +
-                               dp[k + 1][j];
-                    cost = max(cost, mini);
-                }
-                dp[i][j] = cost;
-            }
+        g.push_back(1);
+        
+        for(auto it : nums){
+            g.push_back(it);
         }
 
-        return dp[1][n];
+        g.push_back(1);
+        
+        vector<vector<int>> dp(n+5, vector<int> (n+5, 0));
+ 
+        for(int i = 1; i <= n;i++){
+            dp[i][i] = g[i-1] * g[i] * g[i+1]; 
+        }
+
+        // For Every index assume that you are picking the ith index at last
+
+        int len = 2;
+        while(len <= n){
+            int i = 1;
+            int j = i + len - 1;
+
+            while(j <= n){
+                int mini = INT_MIN;
+                int k = i; // last element decided to destroy
+
+                while(k <= j){
+
+                    int poss = g[i-1] * g[k] * g[j+1] + dp[i][k-1] + dp[k+1][j];
+                    mini = max(mini, poss);
+
+                    k++;
+                }
+                dp[i][j] = mini;
+                i++;
+                j++;
+            }
+
+            len++;
+        }
+
+
+
+        return dp[1][n ];
+
     }
 };
