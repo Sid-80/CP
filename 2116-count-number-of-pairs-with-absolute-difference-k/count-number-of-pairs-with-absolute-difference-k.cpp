5fa145b4ca92770110070a0d634b26b47006844e
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2025-03-14 11:33:16 +0530

    Time: 3 ms (59.15%) | Memory: 16.9 MB (36.77%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -3,16 +3,18 @@ public:
     int countKDifference(vector<int>& nums, int k) {
         unordered_map<int,int> freq;
 
-        int ans = 0;
+        int ans = 0, n = nums.size();
 
-        for(auto it : nums){
-            if(freq.find(it-k) != freq.end()){
-                ans += freq[it-k];
+        for(int i = 0;i < n;i++){
+            freq[nums[i]]++;
+
+            if(freq.find(nums[i]-k) != freq.end()){
+                ans+=freq[nums[i]-k];
             }
-            if(freq.find(it+k) != freq.end()){
-                ans += freq[it+k];
+            if(freq.find(nums[i]+k) != freq.end()){
+                ans+=freq[nums[i]+k];
             }
-            freq[it]++;
+
         }
 
         return ans;
