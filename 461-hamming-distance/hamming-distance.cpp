1e5066e74631187a495430e04eea05461c1e3957
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2025-12-09 22:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (6.59%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans = 0;
+        for(int i = 0;i < 32;i++){
+            if((((x >> i) & 1) == 0) && (((y >> i) & 1) != 0) ){
+                ans +=1;
+            } else if((((x >> i) & 1) != 0) && (((y >> i) & 1) == 0) ){
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+};
