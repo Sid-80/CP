79570b7526f1d253746265cc29ca657c24735be3
Siddharth Bramhecha <101277141+Sid-80@users.noreply.github.com>
2026-07-03 18:54:27 +0530

    Time: 33 ms (5.44%) | Memory: 51.4 MB (5.05%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,33 +1,48 @@
 class Solution {
-
-    long req_time(vector<int>& piles, int i){
-        long t = 0;
-
-        for(auto it:piles){
-            t += ceil((double)it/ (double)i);
-        }
-
-        return t;
-    }
 public:
+    bool canEat(int h, vector<int> b, int k){
+        long long int sum = 0;
+        for (int i = 0; i < b.size(); i++) {
+            if (b[i] % k == 0) {
+                sum += (b[i] / k);
+            } else {
+                sum += (b[i] / k) + 1;
+            }
+        }
+        return sum <= h;
+    }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1;
-        long high = *max_element(piles.begin(),piles.end());
+        int n = piles.size();
 
-        while(l <= high){
-            long m = (l + high)/2;
-            
-            long time = req_time(piles,m);
+        int start = 1;
 
-            if(time <= h){
-                high = m -1;
-            }else{
-                l = m + 1;
+        int end = start;
+
+        for(auto it : piles){
+            end = max(end, it);
+        }
+        
+        int u = 1, ans = -1;
+
+        while(start <= end && u == 1){
+            int mid = (start + end) / 2;
+            if(canEat(h, piles, mid)){
+                if(mid == 1){
+                    ans = 1;
+                    u= 0;
+                }else if(canEat(h, piles, mid - 1) == false){
+                    ans = mid;
+                    u = 0;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                start = mid + 1;
             }
         }
 
-        return l;
+        return ans;
+
     }
-
-
 };
